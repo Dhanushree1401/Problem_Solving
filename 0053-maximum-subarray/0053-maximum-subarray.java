ddfa7f88fda2cfc29397e28710eea31a808f945e
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2024-09-24 12:04:13 +0530

    Time: 52 ms (5.71%), Space: 62.6 MB (5.19%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,26 @@
+import java.util.ArrayList;
 class Solution {
     public int maxSubArray(int[] nums) {
         int curr = nums[0];
         int max = nums[0];
-
+        ArrayList<Integer>gg=new ArrayList<>();
         for (int i = 1; i < nums.length; i++) {
             curr = Math.max(nums[i], curr + nums[i]);
+            gg.add(curr);
             max = Math.max(max, curr);
         }
+        System.out.println(gg);
         return max;
     }
+       /* return gg;*/
+      /*  public ArrayList<Integer> gg(int[] nums) {
+        int curr = nums[0];
+        ArrayList<Integer> gg = new ArrayList<>();
+        gg.add(curr);  
+        
+        for (int i = 1; i < nums.length; i++) {
+            curr = Math.max(nums[i], curr + nums[i]);
+            gg.add(curr); 
+        }
+        return gg;*/
 }
