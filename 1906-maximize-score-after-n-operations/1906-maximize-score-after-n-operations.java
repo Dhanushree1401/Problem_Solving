c12d9e5f47fc8a9fbeff39c26ef08fea2bb8e1ec
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2024-09-29 17:49:19 +0530

    Time: 870 ms (27.15%), Space: 54.8 MB (21.19%) - LeetHub

---
+++ 1906-maximize-score-after-n-operations/1906-maximize-score-after-n-operations.java
@@ -0,0 +1,53 @@
+class Solution {
+    Map<String,Integer>dp=new HashMap<>();
+    int len;
+    public int maxScore(int[] nums) {
+        len=nums.length;
+        char[] tt=new char[len];
+        for(int i=0;i<len;i++)
+        {
+            tt[i]='0';
+        }
+        return dfs(tt,0,nums);
+    }
+    private int dfs(char[] mask,int multi,int[] nums)
+    {
+        String key=new String(mask);
+        if (dp.containsKey(key)) return dp.get(key);
+        if (multi*2==len) return 0; 
+        int res=0;
+        List<Integer> unVisited=getZeros(mask);
+        for (int i = 0; i < unVisited.size()-1;i++) {
+            for (int j=i+1;j<unVisited.size();j++) {
+                int a=unVisited.get(i),b=unVisited.get(j);
+                char[] newMask=mask.clone();
+                newMask[a]='1';
+                newMask[b]='1';
+                res = Math.max(res, dfs(newMask, multi + 1, nums) + gcd(nums[a], nums[b]) * (multi + 1));
+            }
+        }
+        dp.put(key,res);
+        return res;
+    }
+
+    private List<Integer> getZeros(char[] msk) {
+        List<Integer> list=new ArrayList<>();
+        for (int i=0;i<len;i++) {
+            if(msk[i]=='0')
+            {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    private int gcd(int a, int b) {
+        if(a==b) 
+        return a;
+        if (a>b)
+        {
+            return gcd(b,a-b);
+        }
+        return gcd(a,b-a);
+    }
+}
