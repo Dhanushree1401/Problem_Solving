9b5d1d495fddfb7766517ec591e6484f805fa3cc
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2024-12-17 19:56:35 +0530

    Time: 18 ms (80.93%), Space: 45.7 MB (50.42%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.java
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.java
@@ -1,39 +1,25 @@
-import java.util.PriorityQueue;
-
 class Solution {
-    public String repeatLimitedString(String s, int k) {
-        int[] freq = new int[26];
-        for (char c : s.toCharArray()) {
-            freq[c - 'a']++;
-        }
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) pq.offer(new int[] {i + 'a', freq[i]});
-        }
-        
-        StringBuilder result = new StringBuilder();
-        while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-            int ch = current[0], count = current[1];
-            int used = Math.min(k, count);
-            
-            for (int i = 0; i < used; i++) {
-                result.append((char) ch);
+    public String repeatLimitedString(String s, int repeatLimit) {
+        int [] vocab = new int[26]; 
+        for (char ch : s.toCharArray()) vocab[ch - 'a']++;
+        StringBuilder sb = new StringBuilder();
+        int idx = 25;
+        while (idx >= 0) {
+            if (vocab[idx] == 0) {
+                idx--;
+                continue;
             }
-            count -= used;
-            
-            if (count > 0) {
-                if (pq.isEmpty()) break;
-                int[] next = pq.poll();
-                result.append((char) next[0]);
-                next[1]--;
-                
-                if (next[1] > 0) pq.offer(next);
-                pq.offer(new int[] {ch, count});
+            int count = Math.min(vocab[idx], repeatLimit);
+            for (int i = 0 ; i < count ; i++) sb.append((char)(idx + 'a'));
+            vocab[idx] -= count;
+            if (vocab[idx] > 0) {  
+                int next = idx - 1;
+                while (next >= 0 && vocab[next] == 0) next--; 
+                if (next < 0) break;
+                sb.append((char) (next + 'a')); 
+                vocab[next]--;
             }
         }
-        
-        return result.toString();
+        return sb.toString();
     }
 }
