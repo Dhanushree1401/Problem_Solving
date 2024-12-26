63ea2924fc54d44a958f1fa09589f75a2e43d0f0
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2024-12-26 07:02:08 +0530

    Time: 5 ms (87.12%), Space: 44.6 MB (35.83%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,15 +1,39 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        return helper(nums, target, 0, 0);
-    }
-
-    private int helper(int[] arr, int target, int idx, int sum) {
-        if (idx >= arr.length)
-            return sum == target ? 1 : 0;
+        int sum = findSum(nums);
+        int tsum = (sum+target)/2;
+        if((sum+target)%2!=0  || sum<Math.abs(target)) 
+            return 0;
+        return dpPartition(nums, nums.length, tsum);
         
-        int subtract = helper(arr, target, idx + 1, sum - arr[idx]);
-        int add = helper(arr, target, idx + 1, sum + arr[idx]);
+    }
+    int findSum(int[] nums){
+        int sum = 0;
+        for(int i=0; i< nums.length; i++){
+            sum += nums[i];
+        }
+        return sum;
+    }
+    int dpPartition(int[] nums, int N, int sum){
+        int[][] dp = new int[N+1][sum+1];
 
-        return subtract + add;
+        for(int i=0; i<=N; i++){
+            dp[i][0] = 1; 
+        }
+        for(int i=1; i<=sum; i++){
+            dp[0][i] = 0; 
+        }
+
+        for(int i=1; i<=N; i++){
+            for(int j=0; j<=sum; j++){
+                if(nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[N][sum];
+
     }
 }
