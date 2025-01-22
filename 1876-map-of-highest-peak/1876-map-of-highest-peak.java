64d7d45214c3abfbe0e463d927ce49831bd0c4bf
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2025-01-22 16:56:44 +0530

    Time: 49 ms (68.19%), Space: 167.8 MB (39.05%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.java
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.java
@@ -1,34 +1,35 @@
 class Solution {
-  public int[][] highestPeak(int[][] isWater) {
-    final int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-    final int m = isWater.length;
-    final int n = isWater[0].length;
-    int[][] ans = new int[m][n];
-    Arrays.stream(ans).forEach(A -> Arrays.fill(A, -1));
-    Queue<Pair<Integer, Integer>> q = new ArrayDeque<>();
-
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        if (isWater[i][j] == 1) {
-          q.offer(new Pair<>(i, j));
-          ans[i][j] = 0;
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length, n = isWater[0].length;
+        int[][] height = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        
+        // Initialize water cells
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                height[i][j] = -1;
+                if(isWater[i][j] == 1) {
+                    height[i][j] = 0;
+                    q.offer(new int[]{i, j});
+                }
+            }
         }
-
-    while (!q.isEmpty()) {
-      final int i = q.peek().getKey();
-      final int j = q.poll().getValue();
-      for (int[] dir : dirs) {
-        final int x = i + dir[0];
-        final int y = j + dir[1];
-        if (x < 0 || x == m || y < 0 || y == n)
-          continue;
-        if (ans[x][y] != -1)
-          continue;
-        ans[x][y] = ans[i][j] + 1;
-        q.offer(new Pair<>(x, y));
-      }
+        
+        // BFS directions
+        int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        
+        // BFS from water to land
+        while(!q.isEmpty()) {
+            int[] curr = q.poll();
+            int x = curr[0], y = curr[1];
+            for(int[] dir : dirs) {
+                int nx = x + dir[0], ny = y + dir[1];
+                if(nx >= 0 && nx < m && ny >= 0 && ny < n && height[nx][ny] == -1) {
+                    height[nx][ny] = height[x][y] + 1;
+                    q.offer(new int[]{nx, ny});
+                }
+            }
+        }
+        return height;
     }
-
-    return ans;
-  }
 }
