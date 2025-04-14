bc214ed593a740fd9426326bf54972249aca6ad5
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2025-04-14 21:37:20 +0530

    Time: 235 ms (78.21%), Space: 0B (100%) - LeetHub

---
+++ 1801-average-time-of-process-per-machine/1801-average-time-of-process-per-machine.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select machine_id,round(avg(timespent),3) as processing_time from (select machine_id,max(timestamp)-min(timestamp) as timespent from activity group by machine_id,process_id)as t group by machine_id; 
