7d55192f59ed7d28d598a8057815ece1b56a1a80
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2025-08-04 22:57:20 +0530

    Time: 49 ms (76.68%), Space: 55.1 MB (62.92%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.java
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -1,60 +1,21 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        int count=0;
-        int n=fruits.length;
-        if(n==1 || n==2)
+        int start = 0,end = 0;
+        int n = fruits.length,maxLen = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        while(end<n)
         {
-            return n;
+            map.put(fruits[end],map.getOrDefault(fruits[end],0)+1);
+            while(map.size()>=3)
+            {
+                map.put(fruits[start],map.get(fruits[start])-1);
+                if(map.get(fruits[start]) == 0) map.remove(fruits[start]);
+                start++;
+            }
+            int currLen = end-start+1;
+            maxLen = Math.max(maxLen,currLen);
+            end++;
         }
-        int max=2;
-        int b1=fruits[0];
-        int b2=-1;
-        int countb1=1;
-        int countb2=0;
-        int sum=0,i=0;
-        for(int j=1;j<n;j++)
-        {
-            int end=fruits[j];
-            if(end==b1)
-            {
-                countb1++;
-            }
-            else if(end==b2)
-            {
-                countb2++;
-            }
-            else if(countb1==0)
-            {
-                b1=end;
-                countb1++;
-            }
-            else if(countb2==0)
-            {
-                b2=end;
-                countb2++;
-            }
-            else
-            {
-              while(countb1 != 0 && countb2 != 0){
-                    int start = fruits[i];
-                    if(start == b1){
-                        countb1--; 
-                    } else {
-                        countb2--; 
-                    }
-                    i++; 
-                }
-                if(countb1 == 0){
-                    b1 = end;
-                    countb1 = 1;
-                } else {
-                    b2 = end;
-                    countb2 = 1;
-                }
-            }
-            sum = countb1 + countb2;
-            max = Math.max(max, sum);
-        }
-        return max; 
+        return maxLen;
     }
 }
