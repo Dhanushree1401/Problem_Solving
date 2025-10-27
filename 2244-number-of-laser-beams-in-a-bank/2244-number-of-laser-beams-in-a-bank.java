3d04ff04fe9841cb7aef9fc8d04fff4f661c02fa
Dhanushree <144223549+Dhanushree1401@users.noreply.github.com>
2025-10-27 09:07:01 +0530

    Time: 11 ms (80.94%), Space: 45 MB (89.08%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -1,21 +1,16 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        if (bank.length < 2) {
-            return 0;
+        int ans=0, prev=0;
+        final int n=bank[0].length();
+        for(String row : bank){
+            int dev=0;
+            for(int j=0; j<n; j++)
+                dev+=(row.charAt(j)=='1'?1:0);
+            if (dev>0){
+                ans+=dev*prev;
+                prev=dev;
+            }   
         }
-        int solution = 0;
-        int prev = 0;
-        int cur = 0;
-        for (String row : bank) {
-            cur = 0;
-            for (char c : row.toCharArray()) {
-                if (c == '1') {
-                    cur++;
-                }
-            }
-            solution += cur * prev;
-            prev = cur == 0 ? prev : cur;
-        }
-        return solution;
+        return ans;
     }
 }
